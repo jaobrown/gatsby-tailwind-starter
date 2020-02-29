68a4fb444d308ea2a6cbcafb2b79bb71240fb595
Jared Brown <jared.brown@elementthree.com>
2020-02-29 17:52:20 -0500

    Remove extras

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,15 +11,6 @@ const IndexPage = () => (
     <Link to="/page-2/" className={`mb-4`}>
       Go to page 2
     </Link>{" "}
-    <br></br>
-    <p>
-      hello there from a nice p tag <span>in a span</span>
-    </p>
-    <button
-      className={`py-2 px-4 bg-teal-400 text-white rounded-full hover:bg-teal-500 mt-4`}
-    >
-      click me
-    </button>
   </Layout>
 )
 
